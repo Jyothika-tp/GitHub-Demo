dbc374a48ab7e891a76dff9a22c30195d53a1db5
Jyothi-PC <Jyothi-PC@Vittal-PC>
2017-02-02 23:21:00 +0530

    Hi....hello

---
+++ src/abcd/A.java
@@ -0,0 +1,13 @@
+package abcd;
+
+import org.testng.annotations.Test;
+
+public class A
+{
+	@Test
+	public void m()
+	{
+		System.out.println("hi");
+	}
+
+}
---
+++ src/abcd/B.java
@@ -0,0 +1,14 @@
+package abcd;
+
+import org.testng.annotations.Test;
+
+public class B 
+{
+	@Test
+	public void m()
+	{
+		System.out.println("Bye");
+	}
+
+
+}
